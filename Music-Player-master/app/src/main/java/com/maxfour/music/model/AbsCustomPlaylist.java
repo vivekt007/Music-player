df1a6e92faf2d0ac20068f764e8ecccc96698b4c
vivekt007 <vivektigadi59@gmail.com>
2023-10-04 13:55:47 +0530

    Music player

---
+++ Music-Player-master/app/src/main/java/com/maxfour/music/model/AbsCustomPlaylist.java
@@ -0,0 +1,24 @@
+package com.maxfour.music.model;
+
+import android.content.Context;
+import android.os.Parcel;
+
+import androidx.annotation.NonNull;
+
+import java.util.List;
+
+public abstract class AbsCustomPlaylist extends Playlist {
+    public AbsCustomPlaylist(long id, String name) {
+        super(id, name);
+    }
+
+    public AbsCustomPlaylist() {
+    }
+
+    public AbsCustomPlaylist(Parcel in) {
+        super(in);
+    }
+
+    @NonNull
+    public abstract List<Song> getSongs(Context context);
+}
